159306d52a116ebdc080f708f0dc786a503a2afa
aleksitoivanen <aleksi.toivanen10@gmail.com>
2026-01-20 16:39:33 +0200

    uusi

--- moduuli 4 tehtävä 2.py
+++ moduuli 4 tehtävä 2.py
@@ -1,3 +1,13 @@
+#Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi
+# niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
+# Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
+
+
+
+
 tuuma = float(input("syötä tuuma: "))
-sentti= (tuuma*2,54)
-    while tuuma
+sentti= (tuuma*2.54)
+while   tuuma >= 0 :
+        print(tuuma, "tuumaa on: ", sentti, "cm")
+
+        tuuma = float(input("syötä tuuma: "))
