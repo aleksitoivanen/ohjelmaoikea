8cd48fa380e67ec26ebd2312cd2b1479eded410f
aleksitoivanen <aleksi.toivanen10@gmail.com>
2026-01-21 15:29:53 +0200

    uusi

--- moduuli 4 tehtävä 6.py
+++ moduuli 4 tehtävä 6.py
@@ -1,3 +1,18 @@
-import math
+
 import random
 
+N = int(input("Kuinka monta pistettä arvotaan: "))
+
+
+laskuri = 0
+ympyrassa = 0
+
+while laskuri < N:
+    x = random.randint(-1000,1000) / 1000
+    y = random.randint(-1000,1000) / 1000
+
+    if x*x + y*y < 1:
+        ympyrassa += 1
+    laskuri += 1
+pi = 4 * ympyrassa / N
+print("piin likiarvo on:",pi)
