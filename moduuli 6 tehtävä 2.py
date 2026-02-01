43e8c6c2576a88f8e885fad669fc5541d4835813
aleksitoivanen <aleksi.toivanen10@gmail.com>
2026-02-01 17:17:36 +0200

    uusi

--- moduuli 6 tehtävä 2.py
+++ moduuli 6 tehtävä 2.py
@@ -1,8 +1,10 @@
 import random
-sivu=int(input("Syötä nopan sivujen määrä: "))
+
 def noppa():
     return random.randint(1,sivu)
+sivu=int(input("Syötä nopan sivujen määrä: "))
 silmaluku=noppa()
+print(silmaluku)
 while silmaluku != sivu:
     silmaluku=noppa()
     print(silmaluku)
